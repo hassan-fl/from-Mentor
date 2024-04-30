10fc6d81dc5fbdd3f94438affed25ac6eb3e80f0
hassan-fl <154222511+hassan-fl@users.noreply.github.com>
2024-04-30 21:07:40 +0100

    Update main.js

--- main.js
+++ main.js
@@ -17,6 +17,9 @@ window.onresize = ()=>{
 let pressed = false;
 // window.event.cancelBubble = true
 document.addEventListener("click",function(e){
+    if(document.querySelector('.hide').style.display  === 'grid'){
+        [...document.querySelectorAll('.hide')].forEach((ee)=> ee.style.display = 'none')
+    }
     if (e.target.className !== "clicker") return;
     if(!pressed){
         e.target.children[0].innerHTML = '<svg width="10" height="6" xmlns="http://www.w3.org/2000/svg"><path stroke="#686868" stroke-width="1.5" fill="none" d="m1 5 4-4 4 4"/></svg>';
